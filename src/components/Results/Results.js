651f90d2f0253a94a88c7fdd74f3ca5bfc449ae7
Cindy Miao <cindyxmiao@gmail.com>
2021-04-03 16:55:05 -0700

    youtube results toggle

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -5,6 +5,8 @@ import "./Results.css";
 const Results = (props) => {
   const [loading, setLoading] = useState(true);
 
+  const [youtubeResults, setYoutubeResults] = useState(false);
+
   const history = useHistory();
   const backToSearch = useCallback(() => history.push("/"), [history]);
 
@@ -25,22 +27,48 @@ const Results = (props) => {
     setLoading(false);
   }, [props.recipes]);
 
+  const handleToggle = (evt) => {
+    console.log(evt.target.checked);
+    setYoutubeResults(evt.target.checked);
+  };
+
   return (
     <div>
       {!loading && (
         <div className="page-wrapper">
-          <div className="header-wrapper">
-            <h3>Results Page</h3>
-            <button className="back-to-search" onClick={backToSearch}>
-              <p style={{ color: "white" }}>New Search</p>
-            </button>
+          <div className="header-wrapper flex-row">
+            <div className="flex-row">
+              <h3>Results Page</h3>
+              <button className="back-to-search" onClick={backToSearch}>
+                <p style={{ color: "white" }}>New Search</p>
+              </button>
+            </div>
+            <div className="toggle-container">
+              <div className="flex-row">
+                <div style={{ marginRight: "1rem", color: "white" }}>
+                  Youtube results
+                </div>
+                <label class="switch">
+                  <input
+                    type="checkbox"
+                    className="input"
+                    onClick={handleToggle}
+                  ></input>
+                  <span class="slider round"></span>
+                </label>
+              </div>
+            </div>
           </div>
           <ul className="container">
             {props.recipes.results.map((item) => {
               return (
                 <li key={item.href} className="card">
                   <a
-                    href={youtubeBaseURL + item.title.replace(/ /g, "+")}
+                    href={
+                      youtubeResults
+                        ? youtubeBaseURL + item.title.replace(/ /g, "+")
+                        : item.href
+                    }
                     target="blank"
                   >
                     <div className="card-container">
